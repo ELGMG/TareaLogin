5e42c3c5c47c47d684985021dbde3338c9ad92ff
ELGMG <justingvs@gmail.com>
2021-10-21 16:50:05 -0600

    MENU
    aun falta

--- TareaLogin.Android/obj/Debug/110/android/environment.armeabi-v7a.s
+++ TareaLogin.Android/obj/Debug/110/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"a26fee1b-8ad7-423c-8f43-5f9216937c35"
+	.asciz	"b5d59fda-7fb1-4ff5-bdcd-28bc25b3c1bc"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
