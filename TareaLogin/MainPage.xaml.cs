8022f957c64b7a2fdefad42f7c11e1d8de366cc2
Andres Viquez Chavarria <andresvvcc@gmail.com>
2021-10-20 23:08:47 -0600

    first commit
    first commit

---
+++ TareaLogin/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TareaLogin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Menu());
+        }
+    }
+}
