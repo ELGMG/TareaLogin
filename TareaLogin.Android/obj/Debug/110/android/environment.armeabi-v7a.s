528ef2fb96734d5ff96e93f4e123885cc910a2cc
ELGMG <justingvs@gmail.com>
2021-10-22 21:26:29 -0600

    prueba

--- TareaLogin.Android/obj/Debug/110/android/environment.armeabi-v7a.s
+++ TareaLogin.Android/obj/Debug/110/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"a26fee1b-8ad7-423c-8f43-5f9216937c35"
+	.asciz	"db325a4a-eb06-43ca-9930-8edb6e1b79a9"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
